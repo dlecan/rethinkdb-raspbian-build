09b2ea3c4245d0d334c2f3dd1a3175796d608177
Damien Lecan <dev@dlecan.com>
2016-08-26 22:31:48 +0200

    Update runtime dependencies

--- 04-package.sh
+++ 04-package.sh
@@ -3,4 +3,4 @@
 source config.sh
 
 cd upstream
-rpxc sh -c 'sudo checkinstall -D --install=no --nodoc --pkgversion 2.3.4 --pkglicense AGPL-3.0 --maintainer dev@dlecan.com -y'
+rpxc sh -c 'sudo checkinstall -D --install=no --nodoc --pkgversion 2.3.4 --pkglicense AGPL-3.0 --maintainer "Damien Lecan \\<dev@dlecan.com\\>" --requires "libc6,libgcc1,libcurl3,libstdc++6,libprotobuf9,libssl1.0.0,zlib1g,libboost-system1.55.0" -y'
