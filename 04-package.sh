985e5a7e64bc4f037a427bacecd010a20ecdb735
Damien Lecan <dev@dlecan.com>
2016-08-26 21:45:36 +0200

    Update 04-package.sh

--- 04-package.sh
+++ 04-package.sh
@@ -3,4 +3,4 @@
 source config.sh
 
 cd upstream
-rpxc sh -c 'sudo checkinstall -D --install=no --nodoc'
+rpxc sh -c 'sudo checkinstall -D --install=no --nodoc --pkgversion 2.3.4 --pkglicense AGPL-3.0 --maintainer dev@dlecan.com -y'
